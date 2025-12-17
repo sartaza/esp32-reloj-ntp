b6f31fc54c046c5c05db190ea4063165abd1cf56
Salvador Artaza Aristondo <s.artaza@gmail.com>
2025-12-17 23:49:33 +0100

    Add files via upload
    "V1.1: Versión estable con corrección de cortes Bluetooth".

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py - Ajustado para Bilbao, España (UTC+1)
+WIFI_SSID = "TuRed"       # <--- ¡CORRIGE ESTO!
+WIFI_PASSWORD = "TuUsuario"      # <--- ¡CORRIGE ESTO!
+NTP_SERVER = "pool.ntp.org" 
+TZ_OFFSET = 1  # Ajuste UTC + 1 hora
